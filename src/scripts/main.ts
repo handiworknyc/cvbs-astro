53dbb9436cf05cb808bb6e7a2f88260550ce3424
Jesse Rosenfield <jesserosenfield@Jesses-MacBook-Pro-2.local>
2025-09-30 21:17:53 -0600

    fixes

---
+++ src/scripts/main.ts
@@ -0,0 +1,18 @@
+// src/scripts/main.ts
+
+// was: <script type="module" src="/src/scripts/smartimage.ts">
+import "./smartimage.ts";
+
+// was: <script type="module" is:global src="/src/scripts/gsap-global.ts">
+import "./gsap-global.ts";
+
+// was: <script type="module" is:global src="/src/scripts/plugins/nativebootstrap.js">
+import "./plugins/nativebootstrap.js";
+
+// was: <script type="module" is:global src="/src/scripts/nav-collapse.js">
+import "./nav-collapse.js";
+
+// was inline after body:
+import "./plugins/flickity.js";
+import "./flickitys.js";
+import "./theme.js";
